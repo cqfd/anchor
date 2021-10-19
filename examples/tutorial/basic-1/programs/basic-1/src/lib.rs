08c78fdf2393934226caf4c44f5612d9dc587fc3
Alan O'Donnell <alan.m.odonnell@gmail.com>
2021-10-19 11:25:12 -0700

    Example while pairing

--- examples/tutorial/basic-1/programs/basic-1/src/lib.rs
+++ examples/tutorial/basic-1/programs/basic-1/src/lib.rs
@@ -1,6 +1,11 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount},
+};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6Uejn3594k4dQqJu1i977RAxQwXU64PhGKwKdpJffEn7");
 
 #[program]
 mod basic_1 {
@@ -15,6 +20,26 @@ mod basic_1 {
     pub fn update(ctx: Context<Update>, data: u64) -> ProgramResult {
         let my_account = &mut ctx.accounts.my_account;
         my_account.data = data;
+        Ok(())
+    }
+
+    pub fn init_mint(_ctx: Context<InitMint>) -> ProgramResult {
+        Ok(())
+    }
+
+    pub fn mint_some_tokens(ctx: Context<MintSomeTokens>, bump: u8) -> ProgramResult {
+        anchor_spl::token::mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::MintTo {
+                    mint: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.destination.to_account_info(),
+                    authority: ctx.accounts.us.to_account_info(),
+                },
+                &[&[&[bump]]],
+            ),
+            1,
+        )?;
         Ok(())
     }
 }
@@ -32,6 +57,40 @@ pub struct Initialize<'info> {
 pub struct Update<'info> {
     #[account(mut)]
     pub my_account: Account<'info, MyAccount>,
+}
+
+#[derive(Accounts)]
+pub struct InitMint<'info> {
+    #[account(init, payer = user, mint::decimals = 6, mint::authority = us)]
+    pub mint: Account<'info, Mint>,
+
+    pub user: Signer<'info>,
+
+    #[account(seeds = [], bump)]
+    pub us: AccountInfo<'info>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct MintSomeTokens<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+
+    #[account(init, payer = user, associated_token::mint = mint, associated_token::authority = user)]
+    pub destination: Account<'info, TokenAccount>,
+    pub user: Signer<'info>,
+
+    #[account(seeds = [], bump)]
+    pub us: AccountInfo<'info>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
 #[account]
--- examples/tutorial/basic-1/tests/basic-1.js
+++ examples/tutorial/basic-1/tests/basic-1.js
@@ -1,10 +1,24 @@
 const assert = require("assert");
 const anchor = require("@project-serum/anchor");
+const spl = require("@solana/spl-token");
 const { SystemProgram } = anchor.web3;
 
 describe("basic-1", () => {
   // Use a local provider.
   const provider = anchor.Provider.local();
+  const program = anchor.workspace.Basic1;
+  // const connection = new anchor.web3.Connection("http://localhost:8899");
+  // const wallet = anchor.web3.Keypair.generate();
+  // const provider = new anchor.Provider(connection, {
+  //   publicKey: wallet.publicKey,
+  //   async signTransaction(tx) {
+  //     tx.sign(wallet);
+  //     return tx;
+  //   },
+  //   async signAllTransactions(txs) {
+  //     return txs.map(tx => { tx.sign(wallet); return tx; });
+  //   }
+  // }, anchor.Provider.defaultOptions());
 
   // Configure the client to use the local cluster.
   anchor.setProvider(provider);
@@ -12,7 +26,6 @@ describe("basic-1", () => {
   it("Creates and initializes an account in a single atomic transaction (simplified)", async () => {
     // #region code-simplified
     // The program to execute.
-    const program = anchor.workspace.Basic1;
 
     // The Account to create.
     const myAccount = anchor.web3.Keypair.generate();
@@ -21,9 +34,9 @@ describe("basic-1", () => {
     // #region code-simplified
     await program.rpc.initialize(new anchor.BN(1234), {
       accounts: {
-        myAccount: myAccount.publicKey,
-        user: provider.wallet.publicKey,
-        systemProgram: SystemProgram.programId,
+        myAccount: myAccount.publicKey.toString(),
+        user: provider.wallet.publicKey.toString(),
+        systemProgram: SystemProgram.programId.toString(),
       },
       signers: [myAccount],
     });
@@ -45,7 +58,7 @@ describe("basic-1", () => {
     // #region update-test
 
     // The program to execute.
-    const program = anchor.workspace.Basic1;
+    // const program = anchor.workspace.Basic1;
 
     // Invoke the update rpc.
     await program.rpc.update(new anchor.BN(4321), {
@@ -61,5 +74,53 @@ describe("basic-1", () => {
     assert.ok(account.data.eq(new anchor.BN(4321)));
 
     // #endregion update-test
+  });
+
+  it("Can initialize a mint", async () => {
+    let mint = anchor.web3.Keypair.generate();
+    let [us, _bump] = await anchor.web3.PublicKey.findProgramAddress([], program.programId);
+    debugger;
+    await program.rpc.initMint({
+      accounts: {
+        mint: mint.publicKey,
+        user: program.provider.wallet.publicKey,
+        us: us,
+        tokenProgram: spl.TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY
+      },
+      signers: [mint]
+    });
+
+    let mintAccount = await program.provider.connection.getAccountInfo(mint.publicKey);
+    console.log(mintAccount.owner.toString());
+
+    // let destination = anchor.web3.Keypair.generate();
+    let destinationTokenAccount = await spl.Token.getAssociatedTokenAddress(
+      spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+      spl.TOKEN_PROGRAM_ID,
+      mint.publicKey,
+      program.provider.wallet.publicKey
+    );
+
+    debugger;
+    await program.rpc.mintSomeTokens(new anchor.BN(_bump), {
+      accounts: {
+        mint: mint.publicKey,
+        destination: destinationTokenAccount,
+        user: program.provider.wallet.publicKey,
+        us: us,
+        tokenProgram: spl.TOKEN_PROGRAM_ID,
+        associatedTokenProgram: spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY
+      },
+    });
+
+    let destinationTokenAccountInfo = await program.provider.connection.getAccountInfo(destinationTokenAccount);
+
+    debugger;
+    console.log(destinationTokenAccountInfo);
+
   });
 });
